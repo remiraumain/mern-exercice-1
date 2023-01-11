d63efe1f9e231f00d7659a0cbb9bbd82a42fd18d
Rémi Raumain <remiraumain@Mac-mini-de-Remi.local>
2023-01-11 15:39:47 +0100

    Still learning on frontend

---
+++ client/src/components/WidgetWrapper.jsx
@@ -0,0 +1,9 @@
+const { Box } = require("@mui/material")
+const { styled } = require("@mui/system")
+
+const WidgetWrapper = styled(Box)(({ theme }) => ({
+    padding: "1.5rem 1.5rem 0.75rem 1.5rem",
+    backgroundColor: theme.palette.background.alt
+}))
+
+export default WidgetWrapper
